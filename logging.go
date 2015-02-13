55d3b105a0c746a89a8d3718cc1f3f4d68feab5a
Simon Kirkby <tigger@interthingy.com>
2015-02-13 15:24:12 +0800

    add logging file ;)

---
+++ logging.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/op/go-logging"
+	"os"
+)
+
+var logger = logging.MustGetLogger("example")
+
+var format = logging.MustStringFormatter(
+	"%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}",
+)
+
+func LogSetup() {
+	backend1 := logging.NewLogBackend(os.Stderr, "", 0)
+	backend1Formatter := logging.NewBackendFormatter(backend1, format)
+	backend1Leveled := logging.AddModuleLevel(backend1)
+	backend1Leveled.SetLevel(logging.DEBUG, "")
+	logging.SetBackend(backend1Formatter)
+}
