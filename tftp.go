5e360968e3de09e97a05c4e84d59af60ae0ba87a
Simon Kirkby <tigger@interthingy.com>
2015-06-30 17:24:12 +0800

    added tftp locking

--- tftp.go
+++ tftp.go
@@ -29,6 +29,7 @@ func getFile(filename string) (r io.Reader, err error) {
 	data := []byte{}
 	if !ok {
 		storageLock.Lock()
+		defer storageLock.Unlock()
 		logger.Notice("tftp cache loading %s", filename)
 		r, _, err = localConf.fs.Get("/tftp/" + filename)
 		data, err := ioutil.ReadAll(r)
@@ -37,7 +38,6 @@ func getFile(filename string) (r io.Reader, err error) {
 			err = errors.New("Fail")
 			return nil, err
 		}
-		storageLock.Unlock()
 		return bytes.NewBuffer(data), err
 	}
 	data = storage[filename]
