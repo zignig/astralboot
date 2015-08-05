d463acede82cf4192bdd84369ed162378ca0328b
Simon Kirkby <tigger@interthingy.com>
2015-08-05 10:01:00 +0800

    fix spawn issues with meta discovery

--- data_dist/boot/coreos/images/get_latest.sh
+++ data_dist/boot/coreos/images/get_latest.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 
 echo "file system image"
-wget -O coreos_production_pxe_image.cpio.gz http://stable.release.core-os.net/amd64-usr/current/coreos_production_pxe_image.cpio.gz
-eche "boot kernel"
-wget -O coreos_production_pxe.vmlinuz http://stable.release.core-os.net/amd64-usr/current/coreos_production_pxe.vmlinuz
+wget -O coreos_production_pxe_image.cpio.gz http://alpha.release.core-os.net/amd64-usr/current/coreos_production_pxe_image.cpio.gz
+echo "boot kernel"
+wget -O coreos_production_pxe.vmlinuz http://alpha.release.core-os.net/amd64-usr/current/coreos_production_pxe.vmlinuz
 
 
