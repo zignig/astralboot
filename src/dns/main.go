de83073f13659b3765a273b1fc3a78baef22fdd7
Simon Kirkby <tigger@interthingy.com>
2015-10-09 15:07:10 +0800

    testing for backend

--- src/dns/main.go
+++ src/dns/main.go
@@ -5,22 +5,17 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
-	"math/rand"
 	"net"
-	"net/url"
 	"os"
 	"strconv"
 	"strings"
 	"time"
 
-	"github.com/coreos/go-etcd/etcd"
 	"github.com/miekg/dns"
 
-	backendetcd "github.com/skynetservices/skydns/backends/etcd"
 	"github.com/skynetservices/skydns/msg"
 	"github.com/skynetservices/skydns/server"
 	"github.com/skynetservices/skydns/stats"
@@ -74,8 +69,6 @@ func init() {
 
 func main() {
 	flag.Parse()
-	machines := strings.Split(machine, ",")
-	client := newClient(machines, tlspem, tlskey, cacert)
 
 	if nameserver != "" {
 		for _, hostPort := range strings.Split(nameserver, ",") {
@@ -89,9 +82,6 @@ func main() {
 		log.Fatalf("skydns: addr is invalid: %s", err)
 	}
 
-	if err := loadConfig(client, config); err != nil {
-		log.Fatalf("skydns: %s", err)
-	}
 	if err := server.SetDefaults(config); err != nil {
 		log.Fatalf("skydns defaults could not be set from /etc/resolv.conf: %v", err)
 	}
@@ -100,65 +90,8 @@ func main() {
 		config.Local = dns.Fqdn(config.Local)
 	}
 
-	backend := backendetcd.NewBackend(client, &backendetcd.Config{
-		Ttl:      config.Ttl,
-		Priority: config.Priority,
-	})
+	backend := NewBackend()
 	s := server.New(backend, config)
-
-	if discover {
-		go func() {
-			recv := make(chan *etcd.Response)
-			go client.Watch("/_etcd/machines/", 0, true, recv, nil)
-			duration := 1 * time.Second
-			for {
-				select {
-				case n := <-recv:
-					if n != nil {
-						if client := updateClient(n, tlskey, tlspem, cacert); client != nil {
-							backend.UpdateClient(client)
-						}
-						duration = 1 * time.Second // reset
-					} else {
-						// we can see an n == nil, probably when we can't connect to etcd.
-						log.Printf("skydns: etcd machine cluster update failed, sleeping %s + ~3s", duration)
-						time.Sleep(duration + (time.Duration(rand.Float32() * 3e9))) // Add some random.
-						duration *= 2
-						if duration > 32*time.Second {
-							duration = 32 * time.Second
-						}
-					}
-				}
-			}
-		}()
-	}
-
-	if stub {
-		s.UpdateStubZones()
-		go func() {
-			recv := make(chan *etcd.Response)
-			go client.Watch(msg.Path(config.Domain)+"/dns/stub/", 0, true, recv, nil)
-			duration := 1 * time.Second
-			for {
-				select {
-				case n := <-recv:
-					if n != nil {
-						s.UpdateStubZones()
-						log.Printf("skydns: stubzone update")
-						duration = 1 * time.Second // reset
-					} else {
-						// we can see an n == nil, probably when we can't connect to etcd.
-						log.Printf("skydns: stubzone update failed, sleeping %s + ~3s", duration)
-						time.Sleep(duration + (time.Duration(rand.Float32() * 3e9))) // Add some random.
-						duration *= 2
-						if duration > 32*time.Second {
-							duration = 32 * time.Second
-						}
-					}
-				}
-			}
-		}()
-	}
 
 	stats.Collect()  // Graphite
 	server.Metrics() // Prometheus
@@ -166,19 +99,6 @@ func main() {
 	if err := s.Run(); err != nil {
 		log.Fatalf("skydns: %s", err)
 	}
-}
-
-func loadConfig(client *etcd.Client, config *server.Config) error {
-	// Override what isn't set yet from the command line.
-	n, err := client.Get("/"+msg.PathPrefix+"/config", false, false)
-	if err != nil {
-		log.Printf("skydns: falling back to default configuration, could not read from etcd: %s", err)
-		return nil
-	}
-	if err := json.Unmarshal([]byte(n.Node.Value), config); err != nil {
-		return fmt.Errorf("failed to unmarshal config: %s", err.Error())
-	}
-	return nil
 }
 
 func validateHostPort(hostPort string) error {
@@ -192,54 +112,6 @@ func validateHostPort(hostPort string) error {
 
 	if p, _ := strconv.Atoi(port); p < 1 || p > 65535 {
 		return fmt.Errorf("bad port number %s", port)
-	}
-	return nil
-}
-
-func newClient(machines []string, tlsCert, tlsKey, tlsCACert string) (client *etcd.Client) {
-	// set default if not specified in env
-	if len(machines) == 1 && machines[0] == "" {
-		machines[0] = "http://127.0.0.1:4001"
-	}
-	if strings.HasPrefix(machines[0], "https://") {
-		var err error
-		// TODO(miek): machines is local, the rest is global, ugly.
-		if client, err = etcd.NewTLSClient(machines, tlsCert, tlsKey, tlsCACert); err != nil {
-			// TODO(miek): would be nice if this wasn't a fatal error
-			log.Fatalf("skydns: failure to connect: %s", err)
-		}
-		return client
-	}
-	return etcd.NewClient(machines)
-}
-
-// updateClient updates the client with the machines found in v2/_etcd/machines.
-func updateClient(resp *etcd.Response, tlsCert, tlsKey, tlsCACert string) (client *etcd.Client) {
-	machines := make([]string, 0, 3)
-	for _, m := range resp.Node.Nodes {
-		u, e := url.Parse(m.Value)
-		if e != nil {
-			continue
-		}
-		// etcd=bla&raft=bliep
-		// TODO(miek): surely there is a better way to do this
-		ms := strings.Split(u.String(), "&")
-		if len(ms) == 0 {
-			continue
-		}
-		if len(ms[0]) < 5 {
-			continue
-		}
-		machines = append(machines, ms[0][5:])
-	}
-	// When CoreOS (and thus etcd) crash this call back seems to also trigger, but we
-	// don't have any machines. Don't trigger this update then, as a) it
-	// crashes SkyDNS and b) potentially leaves with no machines to connect to.
-	// Keep the old ones and hope they still work and wait for another update
-	// in the future.
-	if len(machines) > 0 {
-		log.Printf("skydns: setting new etcd cluster to %v", machines)
-		return newClient(machines, tlsCert, tlsKey, tlsCACert)
 	}
 	return nil
 }
