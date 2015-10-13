b398e64520ea85611a2808012a766c42fb6c4c4d
Simon Kirkby <tigger@interthingy.com>
2015-10-13 10:37:03 +0800

    working dns server

--- src/astralboot/dns.go
+++ src/astralboot/dns.go
@@ -5,8 +5,6 @@ package main
 // loads local nodes and astralboot dns entries
 
 import (
-	//"github.com/miekg/dns"
-
 	"errors"
 	"github.com/skynetservices/skydns/msg"
 	"github.com/skynetservices/skydns/server"
@@ -22,6 +20,7 @@ type Domain struct {
 
 func NewDomain(prefix string) (d *Domain) {
 	d = &Domain{}
+	msg.PathPrefix = "astralboot"
 	d.prefix = msg.Path(prefix)
 	d.entries = make(map[string]*msg.Service)
 	return d
@@ -29,7 +28,6 @@ func NewDomain(prefix string) (d *Domain) {
 
 func NewDnsServer(c *Config, leases *Store) (d *Domain) {
 	d = NewDomain(c.Domain)
-	msg.PathPrefix = "astralboot"
 	active := leases.leases.Active()
 	// add the astralboot
 	d.Add("astralboot", c.BaseIP.String())
@@ -41,18 +39,18 @@ func NewDnsServer(c *Config, leases *Store) (d *Domain) {
 	d.DNSConfig = &server.Config{
 		Domain:  c.Domain,
 		DnsAddr: c.BaseIP.String() + ":53",
+		Verbose: false,
 	}
 	return d
 }
 
 func (d *Domain) Run() {
 	for i, j := range d.entries {
-		logger.Critical("%s %v", i, j)
+		logger.Info("%s %v", i, j)
 	}
 	server.Metrics()
 	server.SetDefaults(d.DNSConfig)
 	s := server.New(d, d.DNSConfig)
-	logger.Critical("RUN DNS SERVER HERE")
 	s.Run()
 }
 
@@ -74,7 +72,14 @@ func (d *Domain) Add(name string, address string) {
 // skydns interface for serving
 
 func (d *Domain) Records(name string, exact bool) ([]msg.Service, error) {
-	logger.Critical("fetch record %s", name)
+	path, _ := msg.PathWithWildcard(name)
+	logger.Debug("fetch record %s", path)
+	val, ok := d.entries[path]
+	if ok {
+		l := make([]msg.Service, 1)
+		l[0] = *val
+		return l, nil
+	}
 	return nil, errors.New("FAIL")
 }
 
